e28d1fa0b78513c3500412ecac1166526a76afbe
Anirudh-1606 <anirudhsaladi7@gmail.com>
2025-07-14 19:20:16 +0530

    chore: update dependencies in package.json
    - Added new dependencies: @hookform/resolvers, @react-native-async-storage/async-storage, @react-navigation/bottom-tabs, @react-navigation/native, @react-navigation/native-stack, @reduxjs/toolkit, axios, babel-plugin-macros, pouchdb-react-native, react-hook-form, react-native-gesture-handler, react-native-reanimated, react-native-safe-area-context, react-native-screens, react-native-svg, react-native-vector-icons, react-redux, socket.io-client, tailwindcss, twin.macro, yup.
    - Updated devDependencies: added metro-react-native-babel-preset and metro-react-native-babel-transformer.

--- App.tsx
+++ App.tsx
@@ -1,28 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import RootNavigator from './app/navigation/RootNavigator';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
---
+++ app/navigation/RootNavigator.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AuthScreen from '../screens/AuthScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Auth" component={AuthScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
