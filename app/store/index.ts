1775e29fd720a3f07af8b6eedafc063ba3ebd949
Samminga Sainath Rao <sammingasainathrao@gmail.com>
2025-08-06 13:14:41 +0530

    changes

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,12 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import dateReducer from './slices/dateRangeSlice';
+import dateSlice from './slices/dateRangeSlice';
 import listingReducer from './slices/listingSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    date: dateReducer,
+    date: dateSlice,
     listing: listingReducer,
   },
 });
